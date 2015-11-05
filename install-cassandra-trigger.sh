b257e0803b4b257f139a6cb728b3dcc3c11a0b08
touch2shop <php@touch2shop.com>
2015-11-05 16:32:49 +0100

    change bin directory

--- install-cassandra-trigger.sh
+++ install-cassandra-trigger.sh
@@ -5,6 +5,7 @@ set -e
 jar_file="cassandra-logger*.jar"
 settings_file="cassandra-logger.properties"
 gradle_build_dir="build/libs"
+cassandra_bin_dir="/usr/bin"
 
 if [ ! $1 ]; then
 	echo "First argument should be a Cassandra 2.1+ root directory. You need to have write access to it."
@@ -51,7 +52,7 @@ cassandra_pid=`pgrep -u ${user} -f cassandra || true`
 
 if [ ! -z "${cassandra_pid}" ]; then
     echo "Cassandra is running for current user with PID ${cassandra_pid}. Atempting to reload triggers..."
-    if ${cassandra_dir}/bin/nodetool -h localhost reloadtriggers; then
+    if ${cassandra_bin_dir}/nodetool -h localhost reloadtriggers; then
         echo "Trigger loaded successfuly. You can already use it on the CQL sheel."
     else
         echo "Something went wrong. Could not reload triggers. Try restarting Cassandra manually."
